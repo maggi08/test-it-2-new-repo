ba9b17ebf0cd823129750ef69a00596409bcaa2c
Mako <maggi08mus@gmail.com>
2024-02-08 21:28:32 +0600

    aidos removed

--- home-works/aizahan.js
+++ home-works/aizahan.js
@@ -1,5 +1,5 @@
 console.log('Aizhan done with hw');
-console.log('Aizhan done with hw');
+console.log('Aizhan done s with hw');
 console.log('Aizhan done with hw');
 console.log('Aizhan done with hw');
 
