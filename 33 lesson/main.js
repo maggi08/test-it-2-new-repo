e8e28f361fe27e28359187b56e955bfd5a018a0b
Mako <maggi08mus@gmail.com>
2024-02-19 21:33:50 +0600

    done

--- 33 lesson/main.js
+++ 33 lesson/main.js
@@ -1,118 +1,113 @@
-// let btn = document.querySelector('#btn-confirm')
-// let btnCancel = document.querySelector('.btn-cancel')
-// let div = document.querySelector('.hello-msg')
+// click – происходит, когда кликнули на элемент левой кнопкой мыши (на устройствах с сенсорными экранами оно происходит при касании).
+// contextmenu – происходит, когда кликнули на элемент правой кнопкой мыши.
+// mouseover / mouseout – когда мышь наводится на / покидает элемент.
+// mousedown / mouseup – когда нажали / отжали кнопку мыши на элементе.
+// mousemove – при движении мыши.
+// События на элементах управления:
 
-// btn.addEventListener('click', () => {
-//     console.log('btn', btn);
-//     console.log('div', div);
-//     div.style.display = 'block'
-// })
+// submit – пользователь отправил форму <form>.
+// focus – пользователь фокусируется на элементе, например нажимает на <input>.
+// Клавиатурные события:
 
+// keydown и keyup – когда пользователь нажимает / отпускает клавишу.
+// События документа:
 
-// function onClickCancel(){
-//     console.log('onClickCancel');
-//     div.style.display = 'none'
+// DOMContentLoaded – когда HTML загружен и обработан, DOM документа полностью построен и доступен.
+// CSS events:
+
+// transitionend – когда CSS-анимация завершена.
+
+// function countTo3() {
+//   console.log('countTo3');
+//   for (let index = 1; index <= 3; index++) {
+//     alert(index);
+//   }
 // }
-// btnCancel.addEventListener('click', onClickCancel)
+// const btn2 = document.querySelector('#btn2');
+// btn2.onclick = function () {
+//   alert('second alert');
+// };
+// btn2.onclick = countTo3; // countTo3() // undefined
+// // countTo3 - функция
+// // countTo3() - вызов функций
+
+// function first(event) {
+//     console.log(event);
+//   alert('hello');
+// }
+// function second() {
+//   alert('Bye Bye');
+// }
+
+// // addEventListener
+
+// btn2.addEventListener('click', first);
+// btn2.addEventListener('click', second);
+
+// const btn3 = document.querySelector('#btn3');
+// function onclickbtn3(event){
+//     console.log(event.type);
+//     console.log(event.currentTarget);
+// }
+// btn3.addEventListener('click', onclickbtn3)
+// btn3.addEventListener('mouseover', onclickbtn3)
+// // setTimeout(()=>{
+// //     btn3.removeEventListener('click', onclickbtn3)
+// // }, 3000)
+
+// let a = {name: 'a'}
+// let b = {name: 'a'}
+// let c = a
+// a == b // false 
+// a == c // true
 
 
-
-
-// elem.hasAttribute(name) – проверяет наличие атрибута.
-// elem.getAttribute(name) – получает значение атрибута.
-// elem.setAttribute(name, value) – устанавливает значение атрибута.
-// elem.removeAttribute(name) – удаляет атрибут.
-
-// const myInput = document.querySelector('input')
-// console.log(myInput.value);
-// console.log(myInput.placeholder);
-// console.log(myInput.type);
-// console.log(myInput.id);
-// console.log(myInput.hasAttribute('required'));
-// console.log(myInput.setAttribute('required', true));
-// console.log(myInput.getAttribute('placeholder'));
-// console.log(myInput.removeAttribute('placeholder'));
-// myInput.type= 'password'
-
-// let btn = document.querySelector('#show-passsword')
-// let btnCancel = document.querySelector('.btn-cancel')
-// btn.addEventListener('click', () => {
-//     myInput.setAttribute('type', 'text')
+// document.body.addEventListener('mousemove', ()=>{
+//     console.log('mouse is moving');
 // })
-// btnCancel.addEventListener('click', () => {
-//     myInput.setAttribute('type', 'password')
+
+// document.addEventListener('keydown', (event)=>{
+//     console.log(event);
+// })
+
+// let mouseListener = {
+//     handleEvent(event){
+//         // console.log(event.type);
+//         if(event.type === 'mousedown') this.onMouseDown()
+//         if(event.type === 'mouseup') this.onMouseUp()
+//     },
+//     onMouseDown(){
+//         console.log('onMouseDown');
+//     },
+//     onMouseUp(){
+//         console.log('onMouseUp');
+//     }
+// }
+// document.addEventListener('mousedown', mouseListener )
+// document.addEventListener('mouseup', mouseListener )
+
+// document.body.addEventListener('click', {
+//     handleEvent(evt){
+//         console.log('currentTarget', evt.currentTarget); // element где стоит ваш слушатель 
+//         console.log('target', evt.target); // element где событие тригернулась
+//     }
 // })
 
 
+function onClickDiv(evt){
+    console.log('evt', evt );
+    // evt.stopPropagation()
+}
 
-// document.createElement(tag) – создаёт элемент с заданным тегом,
-// document.createTextNode(value) – создаёт текстовый узел (редко используется),
-// elem.cloneNode(deep) – клонирует элемент, если deep==true, то со всеми дочерними элементами.
+// btn.onclick = onClickDiv
 
-// node.append(...nodes or strings) – вставляет в node в конец,
-// node.prepend(...nodes or strings) – вставляет в node в начало,
-// node.before(...nodes or strings) – вставляет прямо перед node,
-// node.after(...nodes or strings) – вставляет сразу после node,
-// node.replaceWith(...nodes or strings) – заменяет node.
-// node.remove() – удаляет node.
-
-// let ul = document.querySelector('ul')
-// ul.append('Appended')
-// ul.append('Appended2')
-// ul.prepend('Prepended')
-// ul.before('Before')
-// ul.after('After')
-// let ol = document.createElement('ol')
-// console.log(ol);
-// ol.innerHTML = '<li>1</li><li>2</li><li>3</li>'
-// console.log(ol);
-// ul.replaceWith(ol)
-// ol.remove()
-
-// "beforebegin" – вставить html непосредственно перед elem,
-// "afterbegin" – вставить html в начало elem,
-// "beforeend" – вставить html в конец elem,
-// "afterend" – вставить html непосредственно после elem.
-// myDiv.insertAdjacentHTML('afterbegin', '<h1>JS is the best</h1>')
-
-
-// elem.classList.add/remove("class") – добавить/удалить класс.
-// elem.classList.toggle("class") – добавить класс, если его нет, иначе удалить.
-// elem.classList.contains("class") – проверка наличия класса, возвращает true/false.
-
-// Element.classList
-// Element.className
-
-// let btn = document.querySelector('.btn-cancel')
-// console.log(btn.className);
-// console.log(btn.classList);
-// setTimeout(()=>{btn.classList.add('btn-success')},1000)
-// setTimeout(()=>{btn.classList.remove('btn-success')},2000)
-
-// setInterval(()=>{
-//     // if(btn.classList.contains('btn-success')){
-//     //     btn.classList.remove('btn-success')
-//     // }else btn.classList.add('btn-success')
-//     btn.classList.toggle('btn-success')
-// }, 1000)
-
-let btn = document.querySelector('button')
-
-
-setInterval(()=>{
-    let styles = getComputedStyle(btn)
-    let width = styles.width
-    let height = styles.height
-    // console.log(btn.style.width, width);
-    // console.log(height);
-    btn.style.width = (+width.replace('px', '') + 10 ) + 'px'
-    btn.style.height = (+height.replace('px', '') + 10 ) + 'px'
-
-
-
-// console.log('styles', styles.display);
-// console.log('styles', styles.color);
-// console.log('styles', styles.background);
-// console.log('styles', styles.borderColor);
-// console.log('styles', styles.fontSize);
-}, 1000)
+form.onclick = function(event) {
+    event.target.style.backgroundColor = 'yellow';
+  
+    // браузеру нужно некоторое время, чтобы зарисовать всё жёлтым
+    setTimeout(() => {
+      alert("target = " + event.target.tagName + 
+      ", this=" + this.tagName); // this -> event.currentTarget
+      event.target.style.backgroundColor = ''
+    }, 0);
+  };
