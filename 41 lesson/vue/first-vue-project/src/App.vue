37255f2b133f36db3f4d4b9567c58307e1b1c264
Mako <maggi08mus@gmail.com>
2024-03-28 21:27:35 +0500

    vue started

---
+++ 41 lesson/vue/first-vue-project/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Hello Vue</h1>
+  
+  <AppCounter />
+
+  <AppCounter />
+
+</template>
+
+<script>
+import AppCounter from './components/AppCounter.vue'
+
+export default {
+  components:{
+    AppCounter
+  },
+  data(){
+    return {
+    }
+  }
+}
+</script>
+
+
+<style>
+
+</style>
---
+++ 41 lesson/vue/first-vue-project/src/components/AppCounter.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="">
+    <h2>Count: {{ counter}}</h2>
+    <button @click="counter++">Click me</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      counter: 0
+    }
+  }
+  
+}
+</script>
+
+<style >
+
+</style>
